175e21d3723dc3a54d39d452fa50d6909d4772f6
Dick Davis <dick@hey.com>
2026-01-19 19:34:56 -0600

    Add configurable options

---
+++ lib/token_authority/configuration.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module TokenAuthority
+  class Configuration
+    attr_accessor :audience_url, :issuer_url, :secret_key, :user_class
+
+    def initialize
+      @audience_url = nil
+      @issuer_url = nil
+      @secret_key = nil
+      @user_class = "User"
+    end
+  end
+
+  class << self
+    def config
+      @config ||= Configuration.new
+    end
+
+    def configure
+      yield(config)
+    end
+  end
+end

05529349adcbbffe488ed18e7b93b8411311208a
Dick Davis <webmaster@dick.codes>
2026-01-20 01:30:54 -0600

    Add scripts for testing manually (#7)

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  mount TokenAuthority::Engine => "/token_authority"
+  mount TokenAuthority::Engine => "/oauth"
 
   resources :users, only: %i[new create]
 
   get "sign-in", to: "sessions#new", as: :sign_in
   post "sign-in", to: "sessions#create"
   delete "sign-out", to: "sessions#destroy", as: :sign_out
+
+  get "callback", to: "callbacks#show", as: :oauth_callback
 
   root "sessions#new"
 end
